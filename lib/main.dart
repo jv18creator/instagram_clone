95838bb59d5ee2433eafc9e30600959b63119914
Jeet Viramgama <jviramgama5@gmail.com>
2023-09-05 12:34:42 +0530

    Init
    Signed-off-by: Jeet Viramgama <jviramgama5@gmail.com>

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const InstagramClone());
+}
+
+class InstagramClone extends StatelessWidget {
+  const InstagramClone({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(color: Colors.redAccent),
+      child: null,
+    );
+  }
+}
